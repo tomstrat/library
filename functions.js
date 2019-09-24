7daac8218dd68975c6cfb1c489b9a00565c5cf42
Tom Strat <tomstratfic@hotmail.com>
2019-09-24 21:29:28 +0100

    add removal of book functionality

--- functions.js
+++ functions.js
@@ -1,10 +1,12 @@
 let myLibrary = [];
+let dataIdIterate = 0;
 
-function Book(title, author, pages, isRead){
+function Book(title, author, pages, isRead, id){
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.isRead = isRead;
+    this.id = id;
 
     this.info = function(){
         let isReadString = isRead ? "read" : "not read yet";
@@ -33,8 +35,9 @@ function addBookToLibrary(){
     let inputRead = document.getElementById("inputRead");
     let isInputRead = inputRead.checked ? true : false;
 
-    let newBook = new Book(inputTitle, inputAuthor, inputPages, isInputRead);
+    let newBook = new Book(inputTitle, inputAuthor, inputPages, isInputRead, dataIdIterate);
     myLibrary.push(newBook);
+    dataIdIterate++;
     render();
 }
 
@@ -52,22 +55,32 @@ function render(){
         let domAuthor = document.createElement('p');
         let domRead = document.createElement('span');
         let domPages = document.createElement('span');
-        //Add Classes
+        let domRemove = document.createElement('button');
+        //Add Classes & Data
         domBook.classList += " book";
         domTitle.classList += " title";
         domAuthor.classList += "author";
         domPages.classList += "pages";
-        domRead.classList += book.isRead ? " read" : " notRead";                   
+        domRead.classList += book.isRead ? " read" : " notRead";
+        domRemove.classList += "remove"; 
+        domBook.setAttribute("data-id", book.id);                  
         //Add innerHTML
         domTitle.innerHTML = book.title;
         domAuthor.innerHTML = book.author;
         domPages.innerHTML = book.pages;
         domRead.innerHTML = book.isRead ? "&#10003;" : "X";
+        domRemove.innerHTML = "X"
+        //Add event listeners
+        domRemove.addEventListener("click", (e)=>{
+            myLibrary = myLibrary.filter(book => book.id != e.target.parentElement.dataset.id);
+            render();
+        });
         //append children
         domBook.appendChild(domTitle);
         domBook.appendChild(domAuthor);
         domBook.appendChild(domRead);
         domBook.appendChild(domPages);
+        domBook.appendChild(domRemove);
         bookshelf.appendChild(domBook);
     });
 }
